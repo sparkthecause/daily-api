92e413929470aff5314bfc7bb7ad565b3ca348fd
Charles Burgess <consulting@cvburgess.com>
2016-01-03 23:00:08 -0500

    add forced SSL

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.disable( 'x-powered-by' );
+
+// Redirect non-HTTPS traffic in production
+if ( process.env.NODE_ENV === 'production' ) {
+
+    var enforce = require('express-sslify');
+    app.use( enforce.HTTPS( true ) );
+
+}
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
