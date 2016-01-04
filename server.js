187d8efdad3e253136af974d3be24326d1bbbdbc
Charles Burgess <consulting@cvburgess.com>
2016-01-03 23:08:16 -0500

    add piping for debugging

--- server.js
+++ server.js
@@ -5,25 +5,29 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-app.disable( 'x-powered-by' );
+if ( process.env.NODE_ENV === "production" || require("piping")() ) {
 
-// Redirect non-HTTPS traffic in production
-if ( process.env.NODE_ENV === 'production' ) {
+  app.disable( 'x-powered-by' );
+
+  // Redirect non-HTTPS traffic in production
+  if ( process.env.NODE_ENV === 'production' ) {
 
     var enforce = require('express-sslify');
     app.use( enforce.HTTPS( true ) );
 
+  }
+
+  app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+  }));
+
+  app.set('view engine', 'hbs');
+
+  app.get('/', function (req, res) {
+    res.render('home');
+  });
+
+  let server = app.listen(3000);
+
 }
-
-app.engine('hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs'
-}));
-
-app.set('view engine', 'hbs');
-
-app.get('/', function (req, res) {
-  res.render('home');
-});
-
-let server = app.listen(3000);
