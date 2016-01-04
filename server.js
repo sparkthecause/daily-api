6393e897a20c5d03f206a70948067b919256fd80
Charles Burgess <consulting@cvburgess.com>
2016-01-03 22:45:04 -0500

    with to express-hbs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 "use strict";
 
-const app = require('express')();
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
