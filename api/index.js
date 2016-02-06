92249c672628a7e0233eb55da24bb9a0f3bea0ca
Charles Burgess <consulting@cvburgess.com>
2016-02-06 18:37:46 -0500

    fix linter issues

--- api/index.js
+++ api/index.js
@@ -6,36 +6,37 @@ const SubscriberHandler = require('./handlers/subscriber');
 
 module.exports = app => {
 
-    const editionHandler = new EditionHandler( app );
-    const subscriberHandler = new SubscriberHandler( app );
+  const editionHandler = new EditionHandler( app );
+  const subscriberHandler = new SubscriberHandler( app );
 
-    router.route('/editions')
-    .get((req, res) => {
+  router.route('/editions')
+  .get((req, res) => {
 
-      editionHandler.editionForDate(req.query.date)
-      .then( result => {
-        res.json(result);
-      })
-      .catch( error => {
+    editionHandler.editionForDate(req.query.date)
+    .then( result => {
+      res.json(result);
+    })
+    .catch( error => {
+      if (error === '404') {
         res.status(404).send('No edition found for that date.');
-      });
-
+      }
     });
+
+  });
 
   router.route('/subscribers')
   .post((req, res) => {
 
-    // editionHandler.editionForDate(req.query.date)
-    // .then( result => {
-    //   res.json(result);
-    // })
-    // .catch( error => {
-    //   res.status(404).send('No edition found for that date.');
-    // });
-    res.send('hi');
+    subscriberHandler.editionForDate(req.query.date)
+    .then( result => {
+      res.json(result);
+    })
+    .catch( error => {
+      res.status(500).send(error);
+    });
 
   });
 
-    return router;
+  return router;
 
 };
