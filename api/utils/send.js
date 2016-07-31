7440fa395a4d543bfb122f11b0d06903308e79ac
Charles Burgess <consulting@cvburgess.com>
2016-07-31 19:33:43 -0400

    replace sendgrid with postmark

--- api/utils/send.js
+++ api/utils/send.js
@@ -1,19 +1,40 @@
 const Promise = require('bluebird');
-const Sendgrid = require('sendgrid');
+const postmark = require('postmark');
 
 module.exports = (app, options) => {
 
   const config = app.get('config');
-  const sendgrid = new Sendgrid(config.sendgrid);
-  const sendEmail = Promise.promisify(sendgrid.send, { context: sendgrid });
+  const client = new postmark.Client(config.postmark);
 
-  return sendEmail({
-    to: options.to,
-    from: config.email.from,
-    fromname: config.email.fromname,
-    subject: options.subject,
-    text: options.text, // https://www.npmjs.com/package/html-to-text
-    html: options.html
+  const messages = [];
+
+  if (Array.isArray(options.to)) {
+
+    for (const recipient of options.to) {
+
+      messages.push({
+        From: config.email.from,
+        To: recipient,
+        Subject: options.subject,
+        TextBody: options.text, // https://www.npmjs.com/package/html-to-text
+        HtmlBody: options.html,
+        TrackOpens: true
+      });
+
+    }
+
+  }
+
+  return new Promise((resolve, reject) => {
+
+    client.sendEmailBatch(messages, (error, success) => {
+
+      if (error) reject(error);
+
+      resolve(success);
+
+    });
+
   });
 
 };
