1f26dffbb37f34c174d2203cd2e7912e3a89a19f
Charles Burgess <consulting@cvburgess.com>
2016-08-03 19:25:51 -0400

    shuffle server.js layout

--- server.js
+++ server.js
@@ -5,23 +5,24 @@ const bodyParser = require('body-parser');
 const config = require('./config');
 
 const app = express();
-app.set('config', config);
+const pg = require('knex')({
+  client: 'pg',
+  connection: config.postgres,
+  debug: config.env === 'development'
+});
 
 app.disable('x-powered-by');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-const pg = require('knex')({
-  client: 'pg',
-  connection: config.postgres,
-  debug: config.isDebug
-});
 app.set('knex', pg);
+app.set('config', config);
 
 // Redirect non-HTTPS traffic in production - TODO: TEMP DISABLED
-// if (config.isProduction) app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// if (config.env === production) app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use('/', require('./api')(app));
 
