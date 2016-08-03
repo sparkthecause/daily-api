7f43e670ad387ceb6fd7552556bcc06e07ac4f20
Charles Burgess <consulting@cvburgess.com>
2016-08-02 22:38:12 -0400

    remove content serving logic

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 // const enforce = require('express-sslify');
 
@@ -24,17 +23,6 @@ app.set('knex', pg);
 // Redirect non-HTTPS traffic in production - TODO: TEMP DISABLED
 // if (config.isProduction) app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: '.hbs'
-});
-
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-
-app.use(express.static('assets'));
-
-app.use('/', require('./www')(app));
-app.use('/api', require('./api')(app));
+app.use('/', require('./api')(app));
 
 app.listen(config.port);
