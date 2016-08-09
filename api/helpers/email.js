cb851a8325bb313bb7d554a4ae2eebbd0264d22b
Charles Burgess <consulting@cvburgess.com>
2016-08-09 18:31:51 -0400

    switch to local files vs CDN

--- api/helpers/email.js
+++ api/helpers/email.js
@@ -1,14 +1,14 @@
 const Mustache = require('mustache');
 const Promise = require('bluebird');
-const request = require('request-promise');
 const inlineCss = require('inline-css');
+const readFile = Promise.promisify(require('fs').readFile);
 const config = require('../../config');
 
 module.exports = class Email {
 
   static blurbToHTML (blurb) {
     blurb.cdn = config.cdn;
-    return request(`${config.cdn}/blurbs/${blurb.blurb_type_id}.mustache`)
+    return readFile(`${__dirname}/../../templates/blurbs/${blurb.blurb_type_id}.mustache`, 'utf8')
     .then(template => Mustache.render(template, blurb));
   }
 
@@ -22,7 +22,7 @@ module.exports = class Email {
     return Promise.all(edition.blurbs.map(blurb => this.blurbToHTML(blurb)))
     .then(blurbs => {
       // Inject blurb snippets into main email template
-      return request(`${config.cdn}/templates/email.mustache`)
+      return readFile(`${__dirname}/../../templates/editions/email.mustache`, 'utf8')
       .then(template => Mustache.render(template, {
         cdn: config.cdn,
         content: blurbs.join('')
