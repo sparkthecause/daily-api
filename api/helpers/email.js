b34faf590ca313c553c33690b198e0746f8add7d
Charles Burgess <consulting@cvburgess.com>
2016-10-22 12:09:31 -0400

    fix lint error

--- api/helpers/email.js
+++ api/helpers/email.js
@@ -6,8 +6,8 @@ const templates = require('daily-templates');
 
 module.exports = class Email {
 
-  static blurbToComponent ({blurb_type, data}) {
-    const templateName = Object.keys(templates).find(tpl => tpl.toLowerCase() === blurb_type);
+  static blurbToComponent (type, data) {
+    const templateName = Object.keys(templates).find(tpl => tpl.toLowerCase() === type);
     const Template = templates[templateName];
     return (Template) ? React.createElement(Template, data) : '';
   }
@@ -19,10 +19,9 @@ module.exports = class Email {
     edition.blurbs.sort((a, b) => a.position - b.position);
 
     // Convert blurbs to React components
-    const blurbComponents = edition.blurbs.map(blurb => this.blurbToComponent(blurb));
+    const blurbComponents = edition.blurbs.map(blurb => this.blurbToComponent(blurb.blurb_type, blurb.data));
     return Promise.join(...blurbComponents)
     .then(blurbs => {
-
       const emailComponent = React.createElement(templates.Email, {
         cssHref: edition.css_href,
         subject: edition.subject
@@ -31,7 +30,6 @@ module.exports = class Email {
       const doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
       const staticMarkup = ReactDOMServer.renderToStaticMarkup(emailComponent);
       return doctype + staticMarkup;
-
     })
     .then(html => inlineCss(html, { url: 'filePath' }));
   }
