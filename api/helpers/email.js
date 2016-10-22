aae98dbe77505b1159fee1f57d0958eebde6bfb4
Charles Burgess <consulting@cvburgess.com>
2016-10-22 12:06:16 -0400

    remove console log

--- api/helpers/email.js
+++ api/helpers/email.js
@@ -30,7 +30,6 @@ module.exports = class Email {
 
       const doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
       const staticMarkup = ReactDOMServer.renderToStaticMarkup(emailComponent);
-      console.log(staticMarkup);
       return doctype + staticMarkup;
 
     })
