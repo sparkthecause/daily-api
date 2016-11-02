9d307bfa024a1752ad6a02769efd978d90ceeba3
Charles Burgess <consulting@cvburgess.com>
2016-11-01 20:03:18 -0400

    add static example of edition

--- api/graphql.js
+++ api/graphql.js
@@ -3,11 +3,25 @@ const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    edition(id: ID, publishDate: String): Edition
+  }
+
+  type Edition {
+    id: ID
+    publishOn: String
+    isApproved: Boolean
   }
 `);
 
-const root = { hello: () => 'Hello world!' };
+const root = {
+  hello: () => 'Hello world!',
+  edition: ({id, publishDate}) => {
+    return {
+    id: 'uuid',
+    publishOn: '2016-12-16',
+    isApproved: false
+  }}
+};
 
 module.exports = graphqlHTTP({
   schema: schema,
