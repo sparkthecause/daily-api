5585c3a3bc7b86da0be3ba155a2a62247c8bebb1
Charles Burgess <consulting@cvburgess.com>
2016-11-03 21:34:54 -0400

    move formatting logic out of model functions

--- api/models/edition.js
+++ api/models/edition.js
@@ -1,27 +1,30 @@
+const formatEditionData = (editionData) => ({
+  id: editionData.edition_id,
+  publishOn: editionData.publish_on,
+  subject: editionData.subject,
+  css: editionData.css_href,
+  approvedAt: editionData.approved_at
+});
+
+const formatBlurbData = (blurbData) => ({
+  id: blurbData.blurb_id,
+  position: blurbData.position,
+  approvedAt: blurbData.approved_at,
+  type: blurbData.blurb_type,
+  data: JSON.stringify(blurbData.data)
+});
+
 const editionModel = {
   editionForID (id, {knex}) {
     return knex.select('*').from('editions').where({ edition_id: id })
     .then(data => {
       if (!data.length) throw new Error(`No edition found for id: ${id}`);
-      const editionData = data[0];
-      return {
-        id: editionData.edition_id,
-        publishOn: editionData.publish_on,
-        subject: editionData.subject,
-        css: editionData.css_href,
-        approvedAt: editionData.approved_at
-      };
+      return formatEditionData(data[0]);
     });
   },
   blurbsForEditionID(editionId, {knex}) {
     return knex.select('*').from('blurbs').where({ edition_id: editionId }).orderBy('position', 'asc')
-    .then(data => data.map(blurbData => ({
-      id: blurbData.blurb_id,
-      position: blurbData.position,
-      approvedAt: blurbData.approved_at,
-      type: blurbData.blurb_type,
-      data: JSON.stringify(blurbData.data)
-    })));
+    .then(data => data.map(blurbData => formatBlurbData(blurbData)));
   }
 };
 
