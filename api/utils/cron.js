b8b284b9e641de0239bbdfeb9fe6f85c390d4042
Charles Burgess <consulting@cvburgess.com>
2016-11-06 16:01:22 -0500

    update cron to use models

--- api/utils/cron.js
+++ api/utils/cron.js
@@ -2,28 +2,26 @@ const Promise = require('bluebird');
 const CronJob = require('cron').CronJob;
 const moment = require('moment');
 
-const EditionHandler = require('../handlers/edition');
-const SubscriberHandler = require('../handlers/subscriber');
-const EmailHelper = require('../helpers/email');
+const model = require('../models');
 const send = require('./send');
 
 module.exports = (app) => {
-  const editionHandler = new EditionHandler(app);
-  const subscriberHandler = new SubscriberHandler(app);
+
+  const context = { knex: app.get('knex') };
 
   return new CronJob({
     cronTime: '00 30 5 * * 1-5',
     onTick: () => {
       // Runs every weekday (Monday through Friday) at 5:30:00 AM.
-      const date = moment().format('YYYY-MM-DD');
+      const today = moment().format('YYYY-MM-DD');
 
-      const editionPromise = editionHandler.editionForDate(date);
-      const htmlPromise = editionPromise.then(edition => EmailHelper.htmlForEdition(edition));
-      const subcribersPromise = subscriberHandler.fetchActiveSubscribers();
+      const editionPromise = model.findEdition({ publishDate: today }, context);
+      const htmlPromise = editionPromise.then(edition => model.renderHTMLForEdition(edition, context));
+      const subcribersPromise = model.findSubscribers({isActive: true}, context);
 
       Promise.join(editionPromise, htmlPromise, subcribersPromise, (edition, html, subscribers) => {
         return send(app, {
-          to: subscribers.map(subscriber => subscriber.email_address),
+          to: subscribers.map(subscriber => subscriber.email),
           subject: edition.subject,
           html
         });
