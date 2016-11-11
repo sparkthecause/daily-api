43fe9321df94366b5977620220f030f39fc657ab
Charles Burgess <consulting@cvburgess.com>
2016-11-10 20:10:57 -0500

    add mergeVars when sending email
    closes #58

--- src/utils/send.js
+++ src/utils/send.js
@@ -1,21 +1,24 @@
 const postmark = require('postmark');
+const { render } = require('mustache');
 
-exports.sendEmails = (config, { html, subject, text, to }) => {
-  const client = new postmark.Client(config.postmark);
+exports.sendEmails = ({ html, mergeVars, subject, text, to }, context) => {
+  const client = new postmark.Client(context.config.postmark);
 
   const messages = [];
 
-  if (Array.isArray(to)) {
-    for (const recipient of to) {
-      messages.push({
-        From: config.email.from,
-        To: recipient,
-        Subject: subject,
-        TextBody: text, // https://www.npmjs.com/package/html-to-text
-        HtmlBody: html,
-        TrackOpens: true
-      });
-    }
+  if (!Array.isArray(to)) {
+    throw new Error('When sending email, `to` must be an array of objects');
+  }
+
+  for (const recipient of to) {
+    messages.push({
+      From: context.config.email.from,
+      To: recipient.email,
+      Subject: subject,
+      TextBody: text, // https://www.npmjs.com/package/html-to-text
+      HtmlBody: render(html, Object.assign(mergeVars, recipient.mergeVars)),
+      TrackOpens: true
+    });
   }
 
   return new Promise((resolve, reject) => {
