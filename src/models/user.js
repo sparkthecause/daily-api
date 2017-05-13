541553d8f8d262b69359402c26a48ef8180bbdf4
Charles Burgess <consulting@cvburgess.com>
2017-05-13 11:49:58 -0400

    add User to graphql

---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+
+const userModel = {
+
+  findUser (userId, { knex }) {
+    return knex.select('*').from('users').where({ userId })
+    .then(userData => (userData.length) ? userData[0] : null);
+  },
+
+  findUsers ({ userIds, canAccess }, { knex }) {
+    return knex.select('*').from('users').modify(queryBuilder => {
+      if (userIds) queryBuilder.whereIn('userId', userIds);
+      if (canAccess !== undefined) queryBuilder.where({ canAccess });
+    });
+  },
+
+  createUser (username, password, optionalData, { knex }) {
+    if (!username || !passport) throw new Error('username and password are required');
+
+    return bcrypt.hash(password, 8)
+    .then(hashedPassword => Object.assign({ username, password: hashedPassword }, optionalData))
+    .then(user => knex.insert(user).into('users').returning('*'))
+    .then(userData => (userData.length) ? userData[0] : null);
+  }
+
+};
+
+module.exports = userModel;
---
+++ src/schemas/user.js
@@ -0,0 +1,50 @@
+const moment = require('moment');
+const models = require('../models');
+
+exports.schema = `
+  type User {
+    userId: ID!
+    username: String!
+    firstName: String
+    lastName: String
+    canAccess: Boolean!
+  }
+`;
+
+exports.query = `
+  user(
+    userId: ID
+  ): User
+
+  users(
+    userIds: [ID]
+    canAccess: Boolean
+  ): [User]
+`;
+
+exports.mutation = `
+  createUser(
+    username: String!
+    password: String!
+    firstName: String
+    lastName: String
+    canAccess: Boolean
+  ): User
+`;
+
+exports.resolvers = {
+  Query: {
+    user (root, { userId }, context) {
+      return models.findUser( userId, context );
+    },
+    users (root, { userIds, canAccess }, context) {
+      return models.findUsers( { userIds, canAccess }, context );
+    }
+  },
+
+  Mutation: {
+    createUser (root, { username, password, firstName, lastName, canAccess }, context) {
+      return models.createUser(username, password, { firstName, lastName, canAccess });
+    }
+  }
+};
